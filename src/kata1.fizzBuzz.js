c3bca850d345792b32e83577d6d7c067d9718966
Ash Askin <ashaskincodes@gmail.com>
2022-03-01 12:43:38 +0000

    FizzBuzz done

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,16 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    if (number % 5 === 0 && number % 3 === 0) {
+        return 'FizzBuzz';
+    }
+    if (number % 5 === 0) {
+        return 'Buzz';
+    }
+    if (number % 3 === 0) {
+        return 'Fizz';
+    }
+    else {
+        return number;
+    }
+};
 
 module.exports = fizzBuzz;
