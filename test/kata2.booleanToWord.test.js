ca3d3760a3c347adf1235100178161e9fd5ea0ae
Ash Askin <ashaskincodes@gmail.com>
2022-03-01 13:08:25 +0000

    Boolean done

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,13 @@
 const { booleanToWord } = require("../src");
 
-describe("booleanToWord", () => {
-  // how do we create specs again???
+describe("fizzBuzz", () => {
+  it("returns Yes when passed true", () => {
+    expect(booleanToWord(7)).toBe('Yes');
+    expect(booleanToWord(true)).toBe('Yes');
+    });
+
+  it("returns No when passed false", () => {
+    expect(booleanToWord(0)).toBe('No');
+    expect(booleanToWord(false)).toBe('No');
+    });
 });
